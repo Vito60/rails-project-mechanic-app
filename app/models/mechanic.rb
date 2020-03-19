1cbac75eda10f245b9e117830f05ada0a0fca346
Vito60 <randallw1996@gmail.com>
2020-03-19 17:58:59 -0400

    added validations for mechanic

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,9 +1,17 @@
 class Mechanic < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :phone_number, presence: true
+    validates :shop_name, presence:true
     has_many :vehicles
     has_many :users, through: :vehicles
 
     def remove_attributes
         self.attributes.delete_if {|attr_name| attr_name == "id" || attr_name == "created_at" || attr_name == "updated_at" }
+    end
+
+    def first_and_last_name
+        self.first_name + " " + self.last_name 
     end
 
 end
