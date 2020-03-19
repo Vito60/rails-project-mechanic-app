4ad0766f4cb72d578bf060a21bd74bac9b1800f6
Vito60 <randallw1996@gmail.com>
2020-03-18 20:46:09 -0400

    added create and destroy for vehicles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
      protect_from_forgery with: :exception
-     before_action :authenticate_user!, :except => [root_path]
+     #before_action :authenticate_user!, :except => [root_path]
 
     def after_sign_in_path_for(resource)
         request.env['omniauth.origin'] || user_path(current_user)
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,9 +1,8 @@
 class VehiclesController < ApplicationController
-    #before_action :authenticate_user_to_customer
+    before_action :authenticate_user_to_vehicle, only: [:show, :destroy]
     
     def index
         @vehicles = current_user.vehicles
-        #binding.pry
     end
     
     def show
@@ -11,10 +10,35 @@ class VehiclesController < ApplicationController
         @vehicle_attributes = @vehicle.delete_vehicle_attributes
     end
 
-    def authenticate_user_to_customer
-        unless current_user.customers.ids.include?(params[:customer_id].to_i)
-            redirect_to user_customers_path(current_user)
+    def new
+        @vehicle = Vehicle.new(user_id: params["user_id"])
+    end
+
+    def create
+        vehicle = Vehicle.new(vehicle_params)
+        if vehicle.save
+            redirect_to user_vehicles_path(current_user)
+        else
+            render 'vehicle/new'
         end
+    end
+
+    def destroy
+        vehicle = Vehicle.find_by_id(params["id"])
+        vehicle.destroy
+        redirect_to user_vehicles_path(current_user)
+    end
+
+    private 
+
+    def authenticate_user_to_vehicle
+        unless current_user.vehicles.ids.include?(params["id"].to_i)
+            redirect_to user_path(current_user)
+        end
+    end
+
+    def vehicle_params
+        params.require(:vehicle).permit(:make, :model, :mileage, :year, :user_id, :mechanic_id)
     end
 
 end
