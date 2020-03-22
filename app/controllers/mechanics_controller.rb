af7a455be37559c5716a225f78adcc1de18f6420
Vito60 <randallw1996@gmail.com>
2020-03-22 16:17:28 -0400

    updated mechanics controller

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,9 +6,31 @@ class MechanicsController < ApplicationController
         @mechanic_attributes = @mechanic.remove_attributes
     end
 
+    def edit
+        @vehicle = Vehicle.find_by_id(params["vehicle_id"])
+        @mechanic = Mechanic.find_by_id(params["id"])
+    end
+
+    def update
+        @mechanic = Mechanic.find_by_id(params["id"])
+        if @mechanic.update(mechanic_params)
+            redirect_to user_vehicle_mechanic_path(current_user, params["vehicle_id"], params["id"])
+        else
+            @mechanic_errors = @mechanic.errors.full_messages
+            render 'mechanics/edit'
+        end
+    end
+
+    private
+
+    def mechanic_params
+        params.require(:mechanic).permit(:first_name, :last_name, :phone_number, :shop_name)
+    end
+
     def authenticate_mechanic_to_vehicle
         unless current_user.mechanics.ids.include?(params["id"].to_i)
             redirect_to user_vehicles_path(current_user)
         end
     end
 end
+
